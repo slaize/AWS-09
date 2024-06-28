b85eb159aeecb8e73095b7bbc97d6d068d20432e
Slaize <slaize@gmail.com>
2024-06-28 10:47:57 +0200

    first Upload

---
+++ flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello World'
+
+@app.route('/secondpage')
+def second_page():
+    return 'This is second page'
+
+@app.route('/thirdpage')
+def third_page():
+    return 'This is third page'
+
+@app.route('/fourth/<string:id>')
+def fourth_page(id):
+    return f'Id of this page is {id}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    # app.run(host='0.0.0.0', port=80)
---
+++ flask-02-Jinja_Template/jinja.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html', number1=5, number2=6)
+
+@app.route('/sum')
+def calculate_sum():
+    number1 = 5
+    number2 = 6
+    total = number1 + number2
+    return render_template('body.html', number1=number1, number2=number2, sum=total)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=30000)
+    # app.run(host= '0.0.0.0', port=80)
